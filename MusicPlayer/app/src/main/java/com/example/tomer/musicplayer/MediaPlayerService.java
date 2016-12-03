14f2bfe0d4d98c2c4e2cbd724d9e8bb8e6aea916
Tomer Mardan <tomer@mardan.co.il>
2016-12-03 15:13:59 +0200

    Notification finished. started Uri

--- MusicPlayer/app/src/main/java/com/example/tomer/musicplayer/MediaPlayerService.java
+++ MusicPlayer/app/src/main/java/com/example/tomer/musicplayer/MediaPlayerService.java
@@ -4,25 +4,35 @@ import android.app.Notification;
 import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
+import android.content.res.AssetFileDescriptor;
 import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.IBinder;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.widget.RemoteViews;
+
+import java.io.IOException;
+
+import static android.content.ContentValues.TAG;
 
 public class MediaPlayerService extends Service {
     MediaPlayer mediaPlayer;
     Notification notification;
-    Intent endIntent;
+    Intent endIntent, pauseIntent, resumeIntent, replayIntent;
     RemoteViews remoteViews;
     final static String EXTRA_RES_ID = "resID";
+    final static String EXTRA_URI = "uri";
     final static String ACTION_END = "end";
     final static String ACTION_PLAY_RES = "playRes";
     final static String ACTION_PLAY_URI = "playUri";
+    final static String ACTION_PAUSE = "pause";
+    final static String ACTION_RESUME = "resume";
+    final static String ACTION_REPLAY = "replay";
 
     /*
-        heiarchy:
-            notificationIntent is inside pendingIntent
-            pendingIntent is inside notification
-            notification also has remoteViews inside of it
+        Hierarchy:
+            notification has remoteViews inside of it
             remoteViews contains the notificationLayout
             the remoteViews also gives the layout action-intents for its views' events
             the action-intents call the service and tells it what to do
@@ -37,72 +47,120 @@ public class MediaPlayerService extends Service {
     public void onCreate() {
         super.onCreate();
         initializeIntents();
-        Intent notificationIntent = new Intent(this, MediaPlayerService.class);
-        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, notificationIntent, 0);
         notification = new Notification.Builder(this)
                 .setSmallIcon(R.mipmap.ic_launcher)
                 .build();
-
         initializeNotiLayout();
     }
+
+
 
     private void initializeNotiLayout() {
         remoteViews = new RemoteViews(getPackageName(), R.layout.notification);
 
         remoteViews.setOnClickPendingIntent(R.id.imgBtnStop,
                 PendingIntent.getService(this, 0, endIntent, 0));
-    }
-    private void updateNotiLayout()
-    {
-
-        notification.contentView = remoteViews;
-
-        startForeground(1, notification);
+        remoteViews.setOnClickPendingIntent(R.id.imgBtnPause,
+                PendingIntent.getService(this, 0, pauseIntent, 0));
+        remoteViews.setOnClickPendingIntent(R.id.imgBtnReplay,
+                PendingIntent.getService(this, 0, replayIntent, 0));
 
     }
+
+
+
+
 
     private void initializeIntents()
     {
         endIntent = new Intent(this,MediaPlayerService.class);
         endIntent.setAction(ACTION_END);
+
+        pauseIntent = new Intent(this,MediaPlayerService.class);
+        pauseIntent.setAction(ACTION_PAUSE);
+
+        resumeIntent= new Intent(this,MediaPlayerService.class);
+        resumeIntent.setAction(ACTION_RESUME);
+
+        replayIntent = new Intent(this,MediaPlayerService.class);
+        replayIntent.setAction(ACTION_REPLAY);
+
     }
+
+
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         switch (intent.getAction())
         {
             case ACTION_PLAY_RES:
-                playResAction(intent);
+            case ACTION_PLAY_URI:
+                playAction(intent);
                 break;
 
-            case ACTION_PLAY_URI:
-                playUriAction(intent);
-                break;
             case ACTION_END:
                 stopSelf(); // calls onDestroy
                 break;
-
-
+            case ACTION_PAUSE:
+                pauseAction();
+                break;
+            case ACTION_RESUME:
+                resumeAction();
+                break;
+            case ACTION_REPLAY:
+                replayAction();
+                break;
         }
-        updateNotiLayout();
-
-
-        //int resId = intent.getIntExtra(EXTRA_RES_ID, 0);
-        //startForeground(1, notification);
-
+        notification.contentView = remoteViews;
+        startForeground(1, notification);
         return super.onStartCommand(intent, flags, startId);
     }
 
-    private void playUriAction(Intent intent) {
-
+    private void replayAction() {
+        mediaPlayer.seekTo(0);
     }
 
-    private void playResAction(Intent intent) {
-        int resId = intent.getIntExtra(EXTRA_RES_ID, 0);
+    private void resumeAction() {
+        mediaPlayer.start();
+        remoteViews.setImageViewResource(R.id.imgBtnPause,R.drawable.pause);
+        remoteViews.setOnClickPendingIntent(R.id.imgBtnPause,
+                PendingIntent.getService(this, 0, pauseIntent, 0));
+    }
+
+    private void pauseAction() {
+        mediaPlayer.pause();
+        remoteViews.setImageViewResource(R.id.imgBtnPause,R.drawable.play);
+        remoteViews.setOnClickPendingIntent(R.id.imgBtnPause,
+                PendingIntent.getService(this, 0, resumeIntent, 0));
+    }
+
+
+
+
+
+
+    private void playAction(Intent intent) {
+        String name ="";
         if (mediaPlayer != null && mediaPlayer.isPlaying())
             mediaPlayer.stop();
-        mediaPlayer = MediaPlayer.create(this, resId);
+        switch (intent.getAction())
+        {
+            case ACTION_PLAY_RES:
+                int resId = intent.getIntExtra(EXTRA_RES_ID, 0);
+                mediaPlayer = MediaPlayer.create(this, resId);
+                name = getResources().getResourceEntryName(resId);
+                break;
+            case ACTION_PLAY_URI:
+                Uri uri = intent.getParcelableExtra(EXTRA_URI);
+                name = uri.toString().replace("file:///", "/");
+                mediaPlayer = MediaPlayer.create(this,Uri.parse(name));
+
+        }
+
+        if (mediaPlayer != null)
         mediaPlayer.start();
+
+        remoteViews.setTextViewText(R.id.tvTitle,name);
     }
 
 
